9f705b5f42e032958f3441be8ddc521c2f701963
Mario2044 <116796406+Mario2044@users.noreply.github.com>
2023-04-26 09:49:05 +0300

    fix

--- index.js
+++ index.js
@@ -1,82 +1,112 @@
-let categoriesList = document.querySelector('.categories ul');
-let productDiv = document.querySelector('.products');
-let productInfoDiv = document.querySelector('.product-info');
-let ordersBtn = document.querySelector('.ordersBtn');
-let ordersEl = document.querySelector('orders');
+const categoriesList = document.querySelector('.categories ul');
+const productsList = document.querySelector('.products ul');
+const productInfo = document.querySelector('.product-info');
+const buyBtn = document.querySelector('.buy-btn');
 
-let products = {
+const products = {
   electronics: [
-    { name: 'Смартфон', price: 5500 },
-    { name: 'Електрочайник', price: 2500 },
-    { name: 'Увлажнитель воздуха', price: 1750 }
+    {name: 'Електрочайник', price: '50 грн'},
+    {name: 'iPhone 14 pro max', price: '15 000 грн'},
+    {name: 'Увлажнитель воздуха', price: '300 грн'}
   ],
-  clothes: [
-    { name: 'Шорты', price: 500 },
-    { name: 'Джинсы', price: 1500 },
-    { name: 'Куртка', price: 1750 }
+  clothing: [
+    {name: 'Носки', price: '20 грн'},
+    {name: 'Рубашки', price: '50 грн'},
+    {name: 'Штаны', price: '200 грн'}
   ],
   books: [
-    { name: 'Звездная Бабочка - Бернар Вербер', price: 400 },
-    { name: 'Мастер на все руки', price: 350 },
-    { name: 'Енциклопедия', price: 750 }
+    {name: 'Звездная бабочка', price: '50 грн'},
+    {name: 'Анатомия и физика', price: '300 грн'},
+    {name: 'Грокаем алгоритмы', price: '150 грн'}
   ]
 };
 
-Object.keys(products).forEach(category => {
-  let li = document.createElement('li');
-  li.textContent = category;
-  li.dataset.category = category;
-  categoriesList.appendChild(li);
+const ordersList = document.querySelector('#orders');
+const myOrdersBtn = document.querySelector('#my-orders');
+
+// при загрузці сторінки перевіряємо, чи є збережені замовлення у localStorage
+const savedOrders = JSON.parse(localStorage.getItem('orders')) || [];
+
+// функція для відображення списку замовлень
+function displayOrders() {
+ordersList.innerHTML = ''; // очищаємо список перед відображенням
+
+savedOrders.forEach((order) => {
+const li = document.createElement('li');
+li.innerHTML = `${order.date} - ${order.price} <button class="delete-order" data-id="${order.id}">видалити</button>`;
+// додаємо обробник на кнопку видалення замовлення
+const deleteBtn = li.querySelector('.delete-order');
+deleteBtn.addEventListener('click', (event) => {
+  const orderId = event.target.dataset.id;
+  deleteOrder(orderId);
 });
 
-categoriesList.addEventListener('click', event => {
-  if (event.target.tagName === 'LI') {
-    let selectCategory = event.target.dataset.category;
-    let selectProducts = products[selectCategory];
-    let productsHtml = selectProducts.map(product => {
-      return `<div class="product" data-name="${product.name}" data-price="${product.price}">
-      <h3>${product.name}</h3>
-      <p>${product.price} грн</p>
-      </div>`;
-    }).join('');
-    productDiv.innerHTML = productsHtml;
-  }
+ordersList.appendChild(li);
 });
-
-productDiv.addEventListener('click', event => {
-  if (event.target.classList.contains('product')) {
-    const name = event.target.dataset.name;
-    const price = event.target.dataset.price;
-    const productInfoHTML = `
-      <h2>${name}</h2>
-      <p>${price} грн</p>
-      <button class="buy">Купить</button>
-    `;
-    productInfoDiv.innerHTML = productInfoHTML;
-  }
-});
-
-productInfoDiv.addEventListener('click', event => {
-  if (event.target.classList.contains('buy')) {
-    alert('Товар куплен');
-    productDiv.innerHTML = '';
-    productInfoDiv.innerHTML = '';
-  }
-});
-
-let orders = JSON.parse(localStorage.getItem("products")) || [];
-
-localStorage.setItem("Basket", JSON.stringify(products));
-
-const buyButton = productInfoDiv.querySelector('.buy');
-
-const product = productDiv.querySelector(".product");
-
-function handleClick() {
-  // Сохраняем информацию о товаре "обувь" в Local Storage
-  localStorage.setItem('selectedItem', product.dataset.name);
-  console.log('Информация о товаре "обувь" сохранена в Local Storage');
 }
 
-// Добавляем обработчик события "click" на кнопку
-buyButton.addEventListener('click', handleClick);
+// функція для додавання нового замовлення
+function addOrder(productName, productPrice) {
+  const newOrder = {
+  id: Date.now(),
+  date: new Date().toLocaleString(),
+  name: productName,
+  price: productPrice,
+  };
+  
+  savedOrders.push(newOrder);
+  localStorage.setItem('orders', JSON.stringify(savedOrders));
+  }
+  
+  // функція для видалення замовлення
+  function deleteOrder(orderId) {
+  const updatedOrders = savedOrders.filter((order) => order.id != orderId);
+  savedOrders = updatedOrders;
+  localStorage.setItem('orders', JSON.stringify(savedOrders));
+  displayOrders();
+  }
+  
+  // додаємо обробник на кнопку "Мої замовлення"
+  myOrdersBtn.addEventListener('click', () => {
+  // показуємо список замовлень
+  document.querySelector('#orders-list').style.display = 'block';
+  // відображаємо збережені замовлення
+  displayOrders();
+  });
+  
+  // додаємо обробник на кнопку "Купити"
+  buyBtn.addEventListener('click', () => {
+  const productName = productInfo.querySelector('h2').textContent;
+  const productPrice = productInfo.querySelector('p').textContent;
+  addOrder(productName, productPrice);
+  });
+
+  // функція для відображення списку продуктів з вибраної категорії
+function displayProducts(categoryName) {
+  productsList.innerHTML = ''; // очищаємо список перед відображенням
+  
+  const categoryProducts = products[categoryName];
+  
+  categoryProducts.forEach((product) => {
+  const li = document.createElement('li');
+  li.innerHTML = `<h3>${product.name}</h3><p>${product.price}</p><button class="add-to-cart">Купити</button>`;
+  // додаємо обробник на кнопку "Купити"
+  const addToCartBtn = li.querySelector('.add-to-cart');
+  addToCartBtn.addEventListener('click', () => {
+  productInfo.querySelector('h2').textContent = product.name;
+  productInfo.querySelector('p').textContent = product.price;
+  buyBtn.style.display = 'block';
+  });
+  productsList.appendChild(li);
+  });
+  }
+
+  buyBtn.addEventListener('click', () => {
+    const productName = productInfo.querySelector('h2').textContent;
+    const productPrice = productInfo.querySelector('p').textContent;
+    addOrder(productName, productPrice);
+    // додаємо вибраний товар до списку замовлень
+    const li = document.createElement('li');
+    li.textContent = `${productName} - ${productPrice}`;
+    ordersList.appendChild(li);
+  });
